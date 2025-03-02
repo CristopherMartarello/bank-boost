0cdee7a26351989627351e8f387243dc83fe7158
CristopherMartarello <crisrossi1313@gmail.com>
2025-03-02 15:40:36 -0300

    feat: adicionando a landing page do bankboost

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var swiper = new Swiper(".slide-depoiments", {
+    slidesPerView: 3,
+    spaceBetween: 32,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
